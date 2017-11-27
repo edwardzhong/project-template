a03ebbc93fe88cdba7cd278d55d0c5f8d155fa1c
edwardzhong <edwardloveyou@gmail.com>
2017-11-27 19:57:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+## 前端快速开发模版
+
+### 工具链
+* 构建工具gulp
+* 编译打包工具browserify
+* 代码版本转化babel
+* css兼容autoprefixer
+* css预编译less
+* html代码预编译模版swig
+* 前端代码版本更新rev
+
+### 公共类库
+* jQuery
+* Zepto
+* 自己开发的单页面框架app.js
+* 图标fontello
+* 字体reboto
+* 基础样式base.css
---
+++ js/app.js
@@ -0,0 +1,66 @@
+// import $ from 'zepto' //zepto没有用 CommonJs 规范的方法 module.exports 来导出模块
+require('../lib/zepto.js');
+import { webApp } from '../lib/app/app.js'
+
+var App=webApp({
+	container:'app',
+	animate:{ effects:['slideInRight', 'slideOutLeft'],delay:600},
+	preLoad:function(){
+
+	},
+	aftLoad:function(){
+		loadPercent(100);
+	}
+})
+.other('/index',{temId:'tempIndex',create:createIndex})
+.when('/button',{temId:'tempButton'})
+.when('/form',{temId:'tempForm'})
+.when('/dialog',{temId:'tempDialog',create:createDialog})
+.init();
+
+function bindEvent(){
+	$('.icon-home').live('click',function(){
+		App.setPage('/index');
+	});
+}
+
+function createIndex(ctx,elem){
+	elem.on('click','[data-hash]',function(){
+		var hash=$(this).attr('data-hash');
+		hash&&ctx.setPage(hash);
+	})
+}
+
+function createDialog(ctx,elem){
+	var load=$('#loading'),
+		fresh=$('#freshing'),
+		share=elem.find('#share'),
+		dialog=elem.find('#dialog');
+	elem.find('[data-tag=loading]').on('click',function(){
+		load.show();
+		loadPercent(30);
+		setTimeout(function() {
+			loadPercent(100);
+		}, 2000);
+	});
+	elem.find('[data-tag=freshing]').on('click',function(){
+		fresh.show();
+		setTimeout(function() {
+			fresh.hide();
+		}, 2000);
+	});
+	elem.find('[data-tag=share]').on('click',function(){
+		share.show();
+	});
+	share.on('click',function(){
+		share.hide();
+	});
+	elem.find('[data-tag=dialog]').on('click',function(){
+		dialog.addClass('active');
+	});
+	dialog.find('a').on('click',function(){
+		dialog.removeClass('active');
+	})
+}
+
+bindEvent();
