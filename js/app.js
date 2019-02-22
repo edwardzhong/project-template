38e39fdd425804fd4512252a5e1128f8bb586c91
edwardzhong <edwardloveyou@gmail.com>
2019-02-22 14:25:41 +0800

    format

--- js/app.js
+++ js/app.js
@@ -1,63 +1,63 @@
 import $ from '../lib/zepto.js';//zepto没有用 CommonJs 规范的方法 module.exports 来导出模块
 import webApp from '../lib/app/app.js';
 
-var App=webApp({
-	container:'app',
-	animate:{ effects:['slideInRight', 'slideOutLeft'],delay:600},
-	preLoad:function(){
+var App = webApp({
+	container: 'app',
+	animate: { effects: ['slideInRight', 'slideOutLeft'], delay: 600 },
+	preLoad: function () {
 
 	},
-	aftLoad:function(){
+	aftLoad: function () {
 		loadPercent(100);
 	}
 })
-.other('/index',{temId:'tempIndex',create:createIndex})
-.when('/button',{temId:'tempButton'})
-.when('/form',{temId:'tempForm'})
-.when('/dialog',{temId:'tempDialog',create:createDialog})
-.init();
+	.other('/index', { temId: 'tempIndex', create: createIndex })
+	.when('/button', { temId: 'tempButton' })
+	.when('/form', { temId: 'tempForm' })
+	.when('/dialog', { temId: 'tempDialog', create: createDialog })
+	.init();
 
-function bindEvent(){
-	$('.icon-home').live('click',function(){
+function bindEvent() {
+	$('.icon-home').live('click', function () {
 		App.setPage('/index');
 	});
 }
 
-function createIndex(ctx,elem){
-	elem.on('click','[data-hash]',function(){
-		var hash=$(this).attr('data-hash');
-		hash&&ctx.setPage(hash);
-	})
+function createIndex(ctx, elem) {
+	elem.on('click', '[data-hash]', function () {
+		var hash = $(this).attr('data-hash');
+		hash && ctx.setPage(hash);
+	});
 }
 
-function createDialog(ctx,elem){
-	var load=$('#loading'),
-		fresh=$('#freshing'),
-		share=elem.find('#share'),
-		dialog=elem.find('#dialog');
-	elem.find('[data-tag=loading]').on('click',function(){
+function createDialog(ctx, elem) {
+	var load = $('#loading'),
+		fresh = $('#freshing'),
+		share = elem.find('#share'),
+		dialog = elem.find('#dialog');
+	elem.find('[data-tag=loading]').on('click', function () {
 		load.show();
 		loadPercent(30);
-		setTimeout(function() {
+		setTimeout(function () {
 			loadPercent(100);
 		}, 2000);
 	});
-	elem.find('[data-tag=freshing]').on('click',function(){
+	elem.find('[data-tag=freshing]').on('click', function () {
 		fresh.show();
-		setTimeout(function() {
+		setTimeout(function () {
 			fresh.hide();
 		}, 2000);
 	});
-	elem.find('[data-tag=share]').on('click',function(){
+	elem.find('[data-tag=share]').on('click', function () {
 		share.show();
 	});
-	share.on('click',function(){
+	share.on('click', function () {
 		share.hide();
 	});
-	elem.find('[data-tag=dialog]').on('click',function(){
+	elem.find('[data-tag=dialog]').on('click', function () {
 		dialog.addClass('active');
 	});
-	dialog.find('a').on('click',function(){
+	dialog.find('a').on('click', function () {
 		dialog.removeClass('active');
 	})
 }
--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 import $ from 'jquery';
-const dialog=$('#dialog'),
-	overlay=$('#overlay'),
-	body=document.body;
+const dialog = $('#dialog'),
+    overlay = $('#overlay'),
+    body = document.body;
 
-$('#showDialog').on('click',show);
-$('#dialog').find('button').on('click',hide);
+$('#showDialog').on('click', show);
+$('#dialog').find('button').on('click', hide);
 
-function show(){
-    dialog.css('display','block');
-    overlay.css('display','block');
-    body.className='modal-open';
-    setTimeout(function() {
-		dialog.addClass('in');
-		overlay.addClass('in');
+function show() {
+    dialog.css('display', 'block');
+    overlay.css('display', 'block');
+    body.className = 'modal-open';
+    setTimeout(function () {
+        dialog.addClass('in');
+        overlay.addClass('in');
     }, 50);
 }
 
-function hide(){
-	dialog.removeClass('in');
-	overlay.removeClass('in');
-    body.className='';
-    setTimeout(function() {
-        dialog[0].style.display='';
-        overlay.css('display','none');
+function hide() {
+    dialog.removeClass('in');
+    overlay.removeClass('in');
+    body.className = '';
+    setTimeout(function () {
+        dialog[0].style.display = '';
+        overlay.css('display', 'none');
     }, 300);
 }
